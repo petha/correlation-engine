97976a34037d99d1124196d2b5e1e0202ca7b67f
Peter Hagström <peter@significantbit.se>
2019-07-02 21:57:31 +0200

    Adding JaCoCo

--- src/main/java/com/github/petha/correlationengine/server/dto/DocumentDTO.java
+++ src/main/java/com/github/petha/correlationengine/server/dto/DocumentDTO.java
@@ -4,13 +4,17 @@ import com.github.petha.correlationengine.model.Document;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @Data
 public class DocumentDTO {
+    @NotBlank
     private UUID id;
+    @NotEmpty
     private Map<String, String> fields = new HashMap<>();
 
     @JsonIgnore
