c0b7b0a9036e67ba8263a3d145303e9511d62d9d
Peter Hagström <peter@significantbit.se>
2019-07-02 22:32:16 +0200

    Adding some validation ruels for DTOs

--- src/main/java/com/github/petha/correlationengine/server/dto/MatchDTO.java
+++ src/main/java/com/github/petha/correlationengine/server/dto/MatchDTO.java
@@ -4,12 +4,15 @@ import com.github.petha.correlationengine.model.Correlation;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
 @Builder
 public class MatchDTO {
+    @NotNull
     private UUID documentId;
+    @NotNull
     private double score;
 
     public static MatchDTO fromCorrelation(Correlation correlation) {
