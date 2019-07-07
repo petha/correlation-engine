dca54437f190bddaed85e9f2fe4aba3e16bb8d7b
Peter Hagström <peter@significantbit.se>
2019-07-07 11:24:32 +0200

    Trying to remove sonarcube analyse of benchmarks

--- src/main/java/com/github/petha/correlationengine/server/dto/AnalyzerDTO.java
+++ src/main/java/com/github/petha/correlationengine/server/dto/AnalyzerDTO.java
@@ -1,12 +1,11 @@
 package com.github.petha.correlationengine.server.dto;
 
-import com.github.petha.correlationengine.model.Analyzer;
 import com.github.petha.correlationengine.extractor.VectorExtractor;
+import com.github.petha.correlationengine.model.Analyzer;
 import com.github.petha.correlationengine.model.Dictionary;
 import lombok.Data;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
